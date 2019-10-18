4f57a9e217885362b042d395e12d0c56049310f4
Валерий Ветчинин <vvetchinin@radiofid.ru>
2019-10-18 17:26:28 +0300

    some diff

--- accelerometer_MMA8653.ino
+++ accelerometer_MMA8653.ino
@@ -1,56 +1,101 @@
-/*
- * MMA845XQ test code
- * (C) 2012 Akafugu Corporation
- *
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- *
- */
-
 
 #include "Wire.h"
 #include "MMA8653.h"
+#include "Servo.h"
 
 MMA8653 accel;
+Servo servoX;
+Servo servoY;
+
+#define SERVO_MIN 0
+#define SERVO_MID 90
+#define SERVO_MAX 180
+#define SERVOX_DELTA 2
+#define SERVOY_DELTA 2
+#define ACC_MIN -64
+#define ACC_MAX  64
+
+int posY = SERVO_MID;
 
 void setup() {
+  pinMode(PC13, OUTPUT);
   Serial.begin(115200);
   Wire.begin();
   accel.begin(false, 2);
-  ///pinMode (6, OUTPUT);
-  //pinMode (9, OUTPUT);
-  //pinMode (7, OUTPUT);
-  //digitalWrite(9, HIGH);
-  //digitalWrite(7, LOW);
+//  servoX.attach(8);
+//  servoY.attach(9);
+  servoX.attach(PB9);
+  servoY.attach(PB8);
+  servoX.write(SERVO_MID);
+  servoY.write(posY);
+  
+  delay(1000);
 }
 
 
 //uint8_t rdata;
 //uint8_t i;
-//int x, x1, y, y1, z, z1;
+int x, y, z;
+int x_old, y_old, z_old;
 //uint8_t stat;
 //String stringOutput;
-long start;
-
+long start=0, s_start=0, led_start=0;
+bool led_on = false;
 //#define TRG 120
 
 void loop() {
-  accel.update();
+  if ((millis() - s_start > 10)) {
+    s_start = millis();
+    accel.update();
+    x = accel.getX();
+    y = accel.getY();
+    x = constrain(x, ACC_MIN, ACC_MAX);
+    y = constrain(y, ACC_MIN, ACC_MAX);
+    if ((x > (x_old+SERVOX_DELTA)) || (x < (x_old-SERVOX_DELTA))){
+      x_old = x;
+      servoX.write(SERVO_MAX - map(x, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX));
+      Serial.print("servoX = ");
+      Serial.println(SERVO_MAX - map(x, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX));
+    }
+    if ((y > (y_old+SERVOY_DELTA)) || (y < (y_old-SERVOY_DELTA))){
+      y_old = y;
+//      if ((y > 2) && (posY > SERVO_MIN)) {
+//        posY--;// = constrain((posY - map(y, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX)), SERVO_MIN, SERVO_MAX);
+//        servoY.write(posY);
+//        Serial.print("servoY = ");
+//        Serial.println(posY);
+//      }
+//      else if ((y < -2) && (posY < SERVO_MAX)) {
+////        posY++;
+//        posY++;// = constrain((posY + map(y, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX)), SERVO_MIN, SERVO_MAX);
+//        servoY.write(posY);
+        Serial.print("servoY = ");
+//        Serial.println(posY);
+//      }
+      servoY.write(SERVO_MAX - map(y, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX));
+      Serial.println(SERVO_MAX - map(y, ACC_MIN, ACC_MAX, SERVO_MIN, SERVO_MAX));
+    }
   //Serial.println("X,\tY,\tZ,\tRho,\tPhi,\tTheta");
   //if (abs(accel.getX()) > TRG || abs(accel.getY()) > TRG || abs(accel.getZ()) > TRG) digitalWrite(6, HIGH); else digitalWrite(6, LOW);
-  if ((millis() - start > 1000)) {
-    start = millis();
-    Serial.print("X = ");
-    Serial.print(accel.getX());
-    Serial.print(" ,Y = ");
-    Serial.print(accel.getY());
-    Serial.print(" ,Z = ");
-    Serial.println(accel.getZ());
+    if ((millis() - start > 1000)) {
+      start = millis();
+      Serial.print("X = ");
+      Serial.print(x);
+      Serial.print(" ,Y = ");
+      Serial.print(y);
+      Serial.print(" ,Z = ");
+      Serial.println(accel.getZ());
+    }
+  }
+  if ((millis() - led_start > 500)) {
+    led_start = millis();
+    if (led_on) {
+      digitalWrite(PC13, HIGH);
+      led_on = false;
+    }
+    else {
+      digitalWrite(PC13, LOW);
+      led_on = true;
+    }
   }
 }
